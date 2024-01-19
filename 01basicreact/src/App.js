4593a1bf918c30d8e7a47ae24b6469f9c8c63a15
suraj2860 <surajkmr20167@gmail.com>
2024-01-19 12:03:14 +0530

    created 2 react apps. one by using Vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>Chai aur React</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+
+  return (
+    <h1>Chai aur React | Suraj</h1>
+  )
+}
+
+export default App
