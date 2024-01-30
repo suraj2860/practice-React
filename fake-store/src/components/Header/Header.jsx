f4091207b10c3bd09f6f29778b65474012c750f5
suraj2860 <surajkmr20167@gmail.com>
2024-01-30 07:52:38 +0530

    fakestore

--- fake-store/src/components/Header/Header.jsx
+++ fake-store/src/components/Header/Header.jsx
@@ -18,7 +18,7 @@ function Header() {
                                 <li><a className="hover:text-gray-200" href="#">Collections</a></li>
                                 <li><a className="hover:text-gray-200" href="#">Contact Us</a></li>
                             </ul>
-                            <div className="hidden xl:flex items-center space-x-5 items-center">
+                            <div className="hidden xl:flex items-center space-x-5">
                                 <a className="hover:text-gray-200" href="#">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
